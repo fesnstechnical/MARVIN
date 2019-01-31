ccf31c00c45c11c9c81e794408a293644a1166a0
Jackson Rushing <jackr14@hotmail.com>
2019-01-31 16:32:32 -0500

    Menu Input beginings
    This is Thomas submitting from jackson's account.
    
    - created test scene for testing menu interaction
    - I know HOW to do it... but i can't since SteamVR input is screwed up (SteamVR_Input._default doesn't work since _default isn't defined... but it is an action set so therefore it should...)
    - talked to alex from the external team. they were facing the same problem and we may need to roll back to steam vr 2.0.0

---
+++ Assets/Scripts/UI/UIInteraction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+public class UIInteraction : MonoBehaviour
+{
+    private void OnCollisionStay(Collision col)
+    {
+        if(col.gameObject.name == "NRUCollider")
+        {
+            if(SteamVR_Input_ActionSet_default.)
+        }
+
+    }
+}
