a53db6602a663530bbe80482131592c511bd53ab
Jackson Rushing <jackr14@hotmail.com>
2019-02-06 12:35:55 -0500

    VR menus 1.0!
    This is Thomas from Jackson's account
    
    - Very basic laser-pointer menu system has been developed
    - (finally) Figured out SteamVR Input
    - menu system only uses 2 scripts
    - the menu test scene has a button that takes you to the NRU scene, but it slows unity to a crawl afterwards...
    - further development invloves being able to turn the laser pointers (and the ability to click buttons) on and off.

---
+++ Assets/Scripts/UI/SceneTransfer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//this script loads the NRU scene.
+//it slows unity down to a crawl after however...
+public class SceneTransfer : MonoBehaviour
+{
+  
+    public void NRUTransfer()
+    {
+        SceneManager.LoadScene("NRU");
+    }
+}
---
+++ Assets/Scripts/UI/VRUIInput.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Valve.VR;
+
+//This script raycasts from the controller to wherever and if it hits a buttton it calls it's onlick function
+//laser pointer is strictly visual.
+
+
+    public class VRUIInput : MonoBehaviour
+    {
+        private void Update()
+        {
+            RaycastHit hit;
+            Ray raycast = new Ray(transform.position, transform.forward);
+            if (SteamVR_Actions._default.InteractUI.GetStateUp(SteamVR_Input_Sources.Any))
+            {
+                Debug.Log("click!");
+                if(Physics.Raycast(raycast,out hit, 300.0f))
+                {
+                    if(hit.transform != null)
+                    {
+                        if(hit.transform.tag == "Button")
+                        {
+                            Debug.Log("that's a button");
+                            hit.transform.GetComponent<Button>().onClick.Invoke();
+                        }
+                    }
+                }
+            }
+
+
+
+
+
+
+
+        }
+    }
