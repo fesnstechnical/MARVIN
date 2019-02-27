d5928fe67fa62c036463fca7d57a7f043aad1cd1
JacksonRushing <jackr14@hotmail.com>
2019-02-27 13:26:15 -0500

    Menu and Questionare Update
    This is Thomas.
    
    + Remorked the menu test scene so now it starts you in a cubicle and you select menu options (just one for now), off of a computer monitor. the menu option still just takes you to the NRU scene (albeit after transfering scenes, the editor slows to a crawl...)
    
    + made a new scene that contains a basic framework for the questionare. currently it's only mouse based, but it can be easily convered to VR now that we have menu input. further work needed here for increasiung text box size and resolution and possibly outputting answers as a text doxument

---
+++ Assets/Scripts/QuestionHandler.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class QuestionHandler : MonoBehaviour
+{
+
+    public Text Qtext;
+    public Button OptionA;
+    public Button OptionB;
+    public Button OptionC;
+    public Button OptionD;
+    private List<char> answers = new List<char>();
+    public int Qnum = 0;
+    void Start()
+    {
+        OptionA.onClick.AddListener(() => { AClick(); });
+        OptionB.onClick.AddListener(() => { BClick(); });
+        OptionC.onClick.AddListener(() => { CClick(); });
+        OptionD.onClick.AddListener(() => { DClick(); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Qnum == 0)
+        {
+            Qtext.GetComponent<Text>().text = "BEEP";
+            OptionA.GetComponentInChildren<Text>().text = "Yes";
+            OptionB.GetComponentInChildren<Text>().text = "No";
+            OptionC.GetComponentInChildren<Text>().text = "What";
+            OptionD.GetComponentInChildren<Text>().text = "DingleBerry!";
+
+        }
+        else if (Qnum == 1)
+        {
+            Qtext.GetComponent<Text>().text = "BOOP";
+            OptionA.GetComponentInChildren<Text>().text = "es";
+            OptionB.GetComponentInChildren<Text>().text = "o";
+            OptionC.GetComponentInChildren<Text>().text = "hat";
+            OptionD.GetComponentInChildren<Text>().text = "ingleBerry!";
+        }
+        else if (Qnum == 2)
+        {
+            Qtext.GetComponent<Text>().text = "BliP";
+            OptionA.GetComponentInChildren<Text>().text = "Yes";
+            OptionB.GetComponentInChildren<Text>().text = "yes";
+            OptionC.GetComponentInChildren<Text>().text = "yEs";
+            OptionD.GetComponentInChildren<Text>().text = "DongleBerry!";
+        }
+        else if (Qnum == 3)
+        {
+            Qtext.GetComponent<Text>().text = "BORP";
+            OptionA.GetComponentInChildren<Text>().text = "No";
+            OptionB.GetComponentInChildren<Text>().text = "No";
+            OptionC.GetComponentInChildren<Text>().text = "No";
+            OptionD.GetComponentInChildren<Text>().text = "DinkleBerg!";
+        }
+        else if (Qnum >= 4)
+        {
+            OptionA.onClick.RemoveAllListeners();
+            OptionB.onClick.RemoveAllListeners();
+            OptionC.onClick.RemoveAllListeners();
+            OptionD.onClick.RemoveAllListeners();
+            
+            Qtext.GetComponent<Text>().text = answers[0] + " " + answers[1] + " " + answers[2] + " " + answers[3];
+            OptionA.GetComponentInChildren<Text>().text = "That's";
+            OptionB.GetComponentInChildren<Text>().text = "It.";
+            OptionC.GetComponentInChildren<Text>().text = "Get";
+            OptionD.GetComponentInChildren<Text>().text = "Out.";
+        }
+
+
+
+    }
+
+    void AClick (){
+        answers.Add('A');
+        Qnum++;
+    }
+
+    void BClick()
+    {
+        answers.Add('B');
+        Qnum++;
+    }
+
+    void CClick()
+    {
+        answers.Add('C');
+        Qnum++;
+    }
+
+    void DClick()
+    {
+        answers.Add('D');
+        Qnum++;
+    }
+}
