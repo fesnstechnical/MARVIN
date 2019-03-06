592dcc4cf7ac35a04ec17729cee40c793e37186b
JacksonRushing <jackr14@hotmail.com>
2019-03-06 13:20:19 -0500

    In game Menu 1.0
    This is Thomas
    
    +added a in game menu to the ERC basement scene that can be accessed by hitting the menu button (the one above the trackpad). it doesn't do anything at the moment, but it will be used as a base for when we have features that need to be toggled on/off

--- Assets/Scripts/UI/VRUIInput.cs
+++ Assets/Scripts/UI/VRUIInput.cs
@@ -2,15 +2,44 @@
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using Valve.VR;
-
+using Valve.VR.InteractionSystem;
+using Valve.VR.Extras;
 //This script raycasts from the controller to wherever and if it hits a buttton it calls it's onlick function
 //laser pointer is strictly visual.
 
 
-    public class VRUIInput : MonoBehaviour
+public class VRUIInput : MonoBehaviour
     {
-        private void Update()
+    public bool menuMode = false;
+    public Canvas menu;
+    private void Update()
         {
+        if (SteamVR_Actions._default.Menu.GetStateUp(SteamVR_Input_Sources.Any))
+        {
+            if (menuMode)
+            {
+                Debug.Log("menu off!!!");
+                menuMode = false;
+            } else
+            {
+                Debug.Log("menu on!!!");
+                menuMode = true;
+            }
+        }
+
+        if (menuMode)
+        {
+            menu.enabled = true;
+            menu.GetComponentInChildren<BoxCollider>().enabled = true;
+            GetComponent<SteamVR_LaserPointer>().thickness = 0.002f;
+        } else
+        {
+            menu.enabled = false;
+            menu.GetComponentInChildren<BoxCollider>().enabled = false;
+            GetComponent<SteamVR_LaserPointer>().thickness = 0.0f;
+        }
+
+
             RaycastHit hit;
             Ray raycast = new Ray(transform.position, transform.forward);
             if (SteamVR_Actions._default.InteractUI.GetStateUp(SteamVR_Input_Sources.Any))
